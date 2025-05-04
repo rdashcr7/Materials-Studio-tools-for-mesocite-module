ae0e5bf60ec17a2bc1dadd924c3c5da870bdb3a4
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 19:43:25 -0400

    connector delete.pl

--- counter ion connector delete.pl
+++ counter ion connector delete.pl
@@ -6,7 +6,7 @@ use strict;
 use Getopt::Long;
 use MaterialsScript qw(:all);
 
-my $doc=$Documents{"BMP-2 KP with CI.xsd"};
+my $doc=$Documents{"Mesostructure.xsd"};
 my $beads=$doc->UnitCell->Sets("S")->Beads;
 my $connectors = $doc->UnitCell->BeadConnectors;
 foreach my $connector (@$connectors) {
