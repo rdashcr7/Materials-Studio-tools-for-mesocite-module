8807cf5b75b88888277494775a683a11a90e0a89
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 19:42:45 -0400

    connector delete.pl

--- counter ion connector delete.pl
+++ counter ion connector delete.pl
@@ -1,4 +1,6 @@
 #!perl
+# This script can be used to delete a connector (or a bond) between any two beads in a mesostructure file (.xsd).
+# Make sure to go to edit sets and chose all beads which can possibly contain the bond or connector of interest. You may chose a mesomoelcule or you can chose all the beads in the entire mesostructure. After selecting, create a new set 'S' for the selected beads.
 
 use strict;
 use Getopt::Long;
@@ -10,5 +12,5 @@ my $connectors = $doc->UnitCell->BeadConnectors;
 foreach my $connector (@$connectors) {
 	my $type1 = $connector->Bead1->BeadTypeName;
     	my $type2 = $connector->Bead2->BeadTypeName;
-    	$connector->Delete if ($type1 eq 'A' || $type2 eq 'A');
+    	$connector->Delete if ($type1 eq 'CA' || $type2 eq 'CB');
 }
