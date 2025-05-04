85c6c32b0ac7121cc2a750664df0383118849686
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 17:27:19 -0400

    Update Connector.pl

--- Connector.pl
+++ Connector.pl
@@ -1,7 +1,8 @@
 #!perl
 # This code can be used to create a connector between two beads 'CA' and 'CB'. This can be useful to create ringed or closed structure within your mesomolecules.
 # Go to edit sets to chose these two specific beads and group them under a new set and name the set 'S'.
-# insert the name of the mesomolecule in line 10.
+# insert the name of the mesomolecule in line 11.
+# After creating the connector, you can rename the beads CA and CB according to your needs. Make sure to declare the forcefield type for the beads after renaming them, not doing so will result in failed jobs. 
 
 use strict;
 use Getopt::Long;
