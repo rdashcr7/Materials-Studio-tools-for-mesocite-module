f7341ecc4b5912d71177ef35383700b34296f9f7
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 17:35:08 -0400

    Add files via upload

---
+++ counter ion connector delete.pl
@@ -0,0 +1,14 @@
+#!perl
+
+use strict;
+use Getopt::Long;
+use MaterialsScript qw(:all);
+
+my $doc=$Documents{"BMP-2 KP with CI.xsd"};
+my $beads=$doc->UnitCell->Sets("S")->Beads;
+my $connectors = $doc->UnitCell->BeadConnectors;
+foreach my $connector (@$connectors) {
+	my $type1 = $connector->Bead1->BeadTypeName;
+    	my $type2 = $connector->Bead2->BeadTypeName;
+    	$connector->Delete if ($type1 eq 'A' || $type2 eq 'A');
+}
