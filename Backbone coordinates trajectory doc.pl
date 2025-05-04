bbd7e79fd00a05b3519ac8a596d7ad3a554e5da5
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 19:46:03 -0400

    Add files via upload

---
+++ Backbone coordinates trajectory doc.pl
@@ -0,0 +1,33 @@
+#!perl
+
+use strict;
+use Getopt::Long;
+use MaterialsScript qw(:all);
+use warnings;  
+
+
+################################################
+
+my $doc = $Documents{"0.1%.xtd"};
+
+my $setName = "Backbone"; # define name of set
+
+my $object = "Beads"; # Set to Atoms or Beads
+
+################################################
+
+my $setItems = $doc->AsymmetricUnit->Sets("$setName")->$object;
+
+for (my $frame = 4001; $frame <= $doc->Trajectory->NumFrames; ++$frame) {
+
+    $doc->Trajectory->CurrentFrame = $frame;
+    #print "Frame $frame\n";
+
+
+    foreach my $item (@$setItems) {
+    
+        # Write out X, Y and Z coordinate
+        printf ("%0.5f %0.5f %0.5f \n", $item->X, $item->Y, $item->Z);
+    }
+    
+}
